28011d942f79d48a43406a6a3d72a6d3fb938d0f
Yerin Koo <yk459@drexel.edu>
2021-04-08 22:56:26 -0400

    .

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,12 @@
 import { gsap } from "gsap";
 
-gsap.to("#red-circle", { duration: 3, x: 200, ease: 'elastic', width: 300});
 
+
+gsap.to("#red-circle", { duration: 3, x: 350});
+gsap.to("#red-circle", { duration: 3, x: 700, rotate: 720, scale: 3, delay:1});
+gsap.to("#red-circle", { duration: 3, x: 200, ease: 'elastic', width: 500, delay:1});
+gsap.to("#red-circle", { duration: 3, x: 500, scale: -3, width:30, delay:3});
+gsap.to("#red-circle", { duration: 2, x: 30, rotate: 90, delay:6});
+gsap.to("#red-circle", { duration: 5, x: 2000, delay:8});
+gsap.to("#red-circle", { duration: 5, x: -150, delay:10});
+gsap.to("#red-circle", { duration: 2, x: -150, rotate: 180, delay:12});
