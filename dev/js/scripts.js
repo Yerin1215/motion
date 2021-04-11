9290c794365bc8823e312c6e743ad4dfa899eeb9
Yerin Koo <yk459@drexel.edu>
2021-04-11 19:26:18 -0400

    animation finished

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -12,19 +12,38 @@
 // gsap.to("#red-circle", { duration: 2, x: -150, rotate: 180, delay:12});
 
 
+// import { gsap } from "gsap";
+// import $ from "jquery";
+
+// let shapeTL = gsap.timeline({paused:true});
+
+// shapeTL.to("#red-circle", { duration: 3, x: 350})
+//     .to("#red-circle", { duration: 3, x: 700, rotate: 720, scale: 3, delay:1})
+// .to("#red-circle", { duration: 3, x: 200, ease: 'elastic', width: 500, delay:1})
+// .to("#red-circle", { duration: 3, x: 500, scale: -3, width:30, delay:3})
+// .to("#red-circle", { duration: 2, x: 30, rotate: 90, delay:6})
+// .to("#red-circle", { duration: 5, x: 2000, delay:8})
+// .to("#red-circle", { duration: 5, x: -150, delay:10})
+// .to("#red-circle", { duration: 2, x: -150, rotate: 180, delay:12});
+
+// $("#red-circle").on("click",function(){
+//     shapeTL.play();
+// })
+
 import { gsap } from "gsap";
-import $ from jsquery;
+import $ from "jquery";
 
 let shapeTL = gsap.timeline({paused:true});
 
-shapeTL.to("#red-circle", { duration: 3, x: 350});
-    .to("#red-circle", { duration: 3, x: 700, rotate: 720, scale: 3, delay:1});
-.to("#red-circle", { duration: 3, x: 200, ease: 'elastic', width: 500, delay:1});
-.to("#red-circle", { duration: 3, x: 500, scale: -3, width:30, delay:3});
-.to("#red-circle", { duration: 2, x: 30, rotate: 90, delay:6});
-.to("#red-circle", { duration: 5, x: 2000, delay:8});
-.to("#red-circle", { duration: 5, x: -150, delay:10});
-.to("#red-circle", { duration: 2, x: -150, rotate: 180, delay:12});
+shapeTL.to("#red-circle", { duration: 1, x: 700})
+    .to("#red-circle", { duration: 1, x: 700, rotate: 720, scale: 3})
+    .to("#red-circle", { duration: 1, x: 200, ease: 'elastic', width: 500})
+    .to("#red-circle", { duration: 1, x: 500, scale: -3, width:30})
+    .to("#red-circle", { duration: 1, x: 30, rotate: 90})
+    .to("#red-circle", { duration: 1, x: 2000})
+    .to("#red-circle", { duration: 1, x: -150})
+    .to("#red-circle", { duration: 1, x: -150, rotate: 180})
+    .to("#red-circle", { duration: 1, ease: 'elastic', width: 60, height: 60});
 
 $("#red-circle").on("click",function(){
     shapeTL.play();
